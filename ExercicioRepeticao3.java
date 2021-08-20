81898cbf4c9191328c63d424fdd2c92da6fb1441
elidio123 <elidio.laureano@fatec.sp.gov.br>
2021-08-20 02:52:14 +0000

    Resolucao repeticao 3

---
+++ ExercicioRepeticao3.java
@@ -0,0 +1,10 @@
+public class ExercicioRepeticao3{
+    public static void main(String args[]){
+        for (int i=1; i<=100; i++){
+             if(i%3==0){           
+             System.out.println("Os multiplos de 3:"+i);
+            } 
+        }
+    }
+}
+//Imprima todos os múltiplos de 3, entre 1 e 100.
